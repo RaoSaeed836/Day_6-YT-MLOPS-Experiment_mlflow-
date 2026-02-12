11c827d65792a2ca5b81111b67955ff784526c3a
RaoSaeed836 <hhuraira564@gmail.com>
2026-02-11 23:31:15 -0800

    First_commit

---
+++ mlruns/4/29e840a1d10a4b9584fd94bced493f38/artifacts/auto_logging.py
@@ -0,0 +1,79 @@
+# hm auto log dono server ma kr skty ha local or remote server dono pr 
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000") 
+
+
+# ya local url tha ab hm apna remote ka url daly gy
+
+# mlflow.set_tracking_uri("https://dagshub.com/RaoSaeed836/Day_6-YT-MLOPS-Experiment_mlflow-.mlflow")
+# import dagshub
+# dagshub.init(repo_owner='RaoSaeed836', repo_name='Day_6-YT-MLOPS-Experiment_mlflow-', mlflow=True)
+
+# import mlflow
+# with mlflow.start_run():
+#   mlflow.log_param('parameter name', 'value')
+#   mlflow.log_metric('metric name', 1)
+
+
+# Load Wine dataset
+wine = load_wine()
+X = wine.data
+y = wine.target
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+
+# Define the params for RF model
+max_depth = 11
+n_estimators = 3
+
+# Mention your experiment below
+mlflow.set_experiment("Data_Pre_processing")
+
+with mlflow.start_run():
+    rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators, random_state=42)
+    rf.fit(X_train, y_train)
+
+    y_pred = rf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    # mlflow.log_metric('accuracy', accuracy)
+    # mlflow.log_param('max_depth', max_depth)
+    # mlflow.log_param('n_estimators', n_estimators)
+
+    # Creating a confusion matrix plot
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=wine.target_names, yticklabels=wine.target_names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('Confusion Matrix')
+
+    # save plot
+    plt.savefig("Confusion-matrix.png")
+
+    # log artifacts using mlflow
+    # mlflow.log_artifact("Confusion-matrix.png")
+
+   # file ko hmy manualy log krwana prta ha ya automaticaly shyd nahe hota 
+    mlflow.log_artifact(__file__)
+
+
+    # tags
+
+   # tag ko hmy manualy log krwana prta ha ya automaticaly  nahe hota kyo ka mlflow ko kia pta kia tag dena ha ya to apke mrze sa hoga na  
+
+    mlflow.set_tags({"Author": 'Vikash', "Project": "Wine Classification"})
+
+    # Log the model
+    # mlflow.sklearn.log_model(rf, "Random-Forest-Model")
+
+    print(accuracy)
